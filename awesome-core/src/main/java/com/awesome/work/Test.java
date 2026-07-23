a22bebb15e425dcff54f5fa553bb924b95031dfb
Rick <1013554704@qq.com>
2026-07-23 21:19:30 +0800

    init3

---
+++ awesome-core/src/main/java/com/awesome/work/Test.java
@@ -0,0 +1,15 @@
+package com.awesome.work;
+
+
+
+/**
+ * @Description
+ * @Author Rick
+ * @Create 2026-07-22 22:11
+ * @email 123456789@qq.com
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
